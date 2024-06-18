6c546174769be61c13544c14f488d91a95f115ee
shrithik185 <shrithik1853@gmail.com>
2024-06-18 09:29:34 +0530

    Initial Commit

---
+++ project3/project3App.swift
@@ -0,0 +1,17 @@
+//
+//  project3App.swift
+//  project3
+//
+//  Created by Hrithik Sharma on 18/06/24.
+//
+
+import SwiftUI
+
+@main
+struct project3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
